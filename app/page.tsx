f4cb1cec5ed525eac1199fa22f9aa27bcda15777
JiLee <moonstar3109@gmail.com>
2026-05-27 22:28:01 +0900

    feat: remove loader from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { Loader } from './_components/Loader'
 import { HeroSection } from './_components/HeroSection'
 import { ServicesSection } from './_components/ServicesSection'
 import { ProjectsSection } from './_components/ProjectsSection'
@@ -8,7 +7,6 @@ import { FooterSection } from './_components/FooterSection'
 export default function Home() {
   return (
     <main className="bg-black">
-      <Loader />
       <div id="intro">
         <HeroSection />
       </div>
